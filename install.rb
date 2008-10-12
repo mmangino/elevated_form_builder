f5aca7908fef1d1e2538cbc1a9f3fc8e0c922d9d
Darren Terhune <darren@headfirstproductions.ca>
2008-10-12 15:55:06 -0700

    sort of added install as a plugin functionality

--- install.rb
+++ install.rb
@@ -1,1 +1,7 @@
+require 'fileutils'
+FileUtils.copy(Dir[File.dirname(__FILE__) + '/lib/elevated_form_builder.rb'], File.dirname(__FILE__) + '/../../../lib/')
+FileUtils.copy(Dir[File.dirname(__FILE__) + '/lib/template_form_builder.rb'], File.dirname(__FILE__) + '/../../../lib/')
+FileUtils.copy(Dir[File.dirname(__FILE__) + '/app/helpers/application_helper.rb'], File.dirname(__FILE__) + '/../../../app/helpers/')
+FileUtils.copy(Dir[File.dirname(__FILE__) + '/examples/_field_with_errors.html.erb'], File.dirname(__FILE__) + '/../../../app/views/partials/forms/')
+FileUtils.copy(Dir[File.dirname(__FILE__) + '/examples/_field.html.erb'], File.dirname(__FILE__) + '/../../../app/views/partials/forms/')
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
