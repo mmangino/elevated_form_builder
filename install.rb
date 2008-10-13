ce2201383cfa07336b388f6619f324ea1de9513f
Darren Terhune <darren@headfirstproductions.ca>
2008-10-12 17:29:55 -0700

    fixed small bug

--- install.rb
+++ install.rb
@@ -1,13 +1,11 @@
 require 'fileutils'
+
 mkdir(File.join(RAILS_ROOT, "app/views/partials/forms")) unless File.exist?(File.join(RAILS_ROOT, "app/views/forms"))
+
 field_file = '_field.html.erb'
-FileUtils.cp_r(
-  File.join(File.dirname(__FILE__), "/lib/forms/_field.html.erb"),
-  File.join(RAILS_ROOT, "app/views/partials/forms/", field_file)
-)
+FileUtils.cp_r(File.join(File.dirname(__FILE__), "/lib/forms/_field.html.erb"), File.join(RAILS_ROOT, "app/views/partials/forms/", field_file))
+
 field_with_errors_file = '_field_with_errors.html.erb'
-FileUtils.cp_r(
-  File.join(File.dirname(__FILE__), "/lib/forms/_field_with_errors.html.erb")
-  File.join(RAILS_ROOT, "app/views/partials/forms/", field_with_errors)
-)
+FileUtils.cp_r(File.join(File.dirname(__FILE__), "/lib/forms/_field_with_errors.html.erb"), File.join(RAILS_ROOT, "app/views/partials/forms/", field_with_errors))
+
 puts IO.read(File.join(File.dirname(__FILE__), 'README'))
